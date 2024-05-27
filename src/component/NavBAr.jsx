2a8191664ff80f5ddc8d9e9b9ab08118b441d32d
Jishnu Gopalan K P <55798251+jishnugopalan@users.noreply.github.com>
2024-05-27 22:35:00 +0530

    updates after front page

--- src/component/NavBAr.jsx
+++ src/component/NavBAr.jsx
@@ -144,31 +144,8 @@ const NavBAr = () => {
               </li>
 
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/jobs">AI Job</a>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/News">News</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-           ADD
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/addJob">Job</a></li>
-            
-            <li><a class="dropdown-item" href="/addprop">Property</a></li>
-            <li><a class="dropdown-item" href="/addservices">Services</a></li>
-          </ul>
-        </li>
-        <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle text-black" href="" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="dropdown1"><CgProfile className="text-black" /></a>
-                <div className="dropdown-menu" aria-labelledby="dropdown1">
-                  <Link className="dropdown-item text-black" to="/profile">PROFILE</Link>
-                  <Link className="dropdown-item text-black" to="/updatepass">CHANGE PASSWORD</Link>
-                  <Link className="dropdown-item text-black" to="/Forgotpass">FORGOT PASSWORD</Link>
-                </div>
-
-              </li>
-
+                <a className="nav-link active" aria-current="page" href="/jobs">AI Job</a></li>
+       
               <div className="ms-auto d-flex">
                 <li className="nav-item dropdown">
                   <a className="nav-link dropdown-toggle text-black" href="" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="dropdown1"><CgProfile className="text-black" /></a>
