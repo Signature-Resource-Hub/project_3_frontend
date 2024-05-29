801f203a966d0083cd7c43d7c3bdc1c52a214b97
Jishnu Gopalan K P <55798251+jishnugopalan@users.noreply.github.com>
2024-05-29 22:20:30 +0530

    Revert "Merge branch 'services' of https://github.com/Signature-Resource-Hub/project_3_frontend"
    This reverts commit 6e139c1920a0d2b55d441863e095aee45094bb6e, reversing
    changes made to 2a8191664ff80f5ddc8d9e9b9ab08118b441d32d.

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ import ViewNurse from './component/AddServices/ViewNurse';
 import ViewPet from './component/AddServices/ViewPet';
 import ViewTravel from './component/AddServices/ViewTravel';
 import ViewLegal from './component/AddServices/ViewLegal';
-import Verify from './component/LoginSignup/Verify';
 
 
 function App() {
@@ -61,8 +60,6 @@ function App() {
         <Route path='/viewallj' element={<ViewAllJob/>}/>
         <Route path='/jobs' element={<Jobs/>}/>
         <Route path='/LoginSignup' element={<LoginSignup/>}/>
-        <Route path='/verify' element={<Verify/>}/>
-        
         <Route path='/ViewjobbyId' element={<ViewIdJob/>}/>
         <Route path='/contactus' element={<ContactUs/>}/>
         <Route path='/about' element={<About/>}/>
